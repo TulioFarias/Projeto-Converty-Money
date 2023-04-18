75ef026630a290ecff19e5201dfca38ff2945d57
TulioFarias <tuliott10@gmail.com>
2023-04-18 11:22:32 -0300

    update api convert

--- scripts.js
+++ scripts.js
@@ -1,25 +1,20 @@
 const button = document.getElementById('convert-button')
 const select = document.getElementById('currency-select')
 
-const dolar = 5.2
-const euro = 5.9
-const bitcoin = 0.0000071
 
 
-
-
-
-
-
-const convertValues = () => {
+const convertValues =  async () => {
     const inputReais = document.getElementById('input-real').value
     const realValuetext= document.getElementById('real-value-text')
     const realValueText2= document.getElementById('currency-value-text')
 
-    const realvalue =
+    const data =  await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then( response => response.json())
+
+    const dolar = data.USDBRL.high
+    const euro = data.EURBRL.high
+    const bitcoin = data.BTCBRL.high
     
-    
-    
+ 
 
     realValuetext.innerHTML = new Intl.NumberFormat('pt-BR',
     { style: 'currency', currency: 'BRL' }
